33343b3205e13b1951f7470e520e74a1b224a566
Alexandru Iaru <alexandru.iaru@code11.com>
2021-05-23 11:53:42 +0300

    Persons photos

--- service/src/helpers/trips-data.ts
+++ service/src/helpers/trips-data.ts
@@ -5,12 +5,28 @@ import beneficiaries from "../resources/beneficiaries.json";
 import trips from "../resources/trips.json";
 
 const result = _.range(10).map((index) => {
-  return {
+  const obj = {
     donor: donors[index],
     beneficiary: beneficiaries[index],
     donation: donations[index],
-    trip: { ...trips[index], mapsImageUrl: `/trips/${index}.png` },
+    trip: {
+      ...trips[index],
+      mapsImageUrl: `http://localhost:3000/trips/${index}.png`,
+    },
   };
+  if (index < 5) {
+    // @ts-ignore
+    obj.donor.imageUrl = `http://localhost:3000/donors/${index}.jpg`;
+  }
+
+  if (index % 2 === 0) {
+    // @ts-ignore
+    obj.beneficiary.imageUrl = `http://localhost:3000/beneficiaries/${
+      index / 2
+    }.jpg`;
+  }
+
+  return obj;
 });
 
 export const tripsData = result;
