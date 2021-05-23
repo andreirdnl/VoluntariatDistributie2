77d2c435d298f185a5e078f7d082d305f02f6a5a
Mircea <mircea@code11.com>
2021-05-23 12:53:26 +0300

    upadted maps photos

--- web-app/src/views/tripConfirmation/index.tsx
+++ web-app/src/views/tripConfirmation/index.tsx
@@ -1,26 +1,35 @@
-  import { Donor } from '../../components/Donor'
-  import { Beneficiary } from '../../components/Beneficiary'
-  import { LocationMap } from '../../components/LocationMap'
-  
-  export const TripConfirmationView: view = ({
-    data = observe.finishedItem,
-  }) => {
-    const {donor, beneficiary, donation, trip} = data
-    const {name, city, streetAddress} = donor
-    const thankYouMessage = 
-    `iti multumim ca te-ai oferit sa transporti ${donation.name}, de la ${name} din ${city}, ${streetAddress}`
-    return (
-      <div className="multumim">
-        <div className="mName">Multumim pentru ajutor</div>
-        <div className="mDesc">{thankYouMessage}</div>
-        <div className="Card">In medie 60% din donatii gasesc un livrator</div>
-        <div className="people">
-            <Donor donator={donor}/>
-            <img src='' />
-            <Beneficiary beneficiar={beneficiary}/>
+import { Donor } from '../../components/Donor'
+import { Beneficiary } from '../../components/Beneficiary'
+import { LocationMap } from '../../components/LocationMap'
+import style from './style.module.css'  
+import { ReactComponent as SVGChevron } from '../../assets/chevron.svg' 
+
+export const TripConfirmationView: view = ({
+  data = observe.finishedItem,
+}) => {
+  console.log(data)
+  const {donor, beneficiary, donation, trip} = data
+  const {name, city, streetAddress} = donor
+  return (
+    <div className={style.confirmation}>
+      <div className="wrapper">
+        <h1>Multumim pentru ajutor</h1>
+        <p className={style.summary}>
+          Iti multumim ca te-ai oferit sa transporti <b>{donation.name}</b>, 
+          de la <b>{name}</b> din <b>{city}</b>, <b>{streetAddress}</b>&nbsp;
+          pana la <b>{beneficiary.name}</b> din <b>{beneficiary.city}</b>, <b>{beneficiary.streetAddress}</b>
+        </p>
+        <div className={style.people}>
+          <Donor donator={donor}/>
+          <SVGChevron />
+          <Beneficiary beneficiar={beneficiary}/>
         </div>
+        {/* <div className={style.facts}>
+          <small>Stiai ca</small>
+          In medie 60% din donatii gasesc un livrator
+        </div> */}
         <LocationMap trip={trip}/>
       </div>
-    )
-  };
-  
+    </div>
+  )
+};
